792f1abf23892b092714ea6675d91a89d5b8bf97
rinku-kr <rk24736599@gmail.com>
2023-12-30 22:40:11 +0530

    Adding a new item screen

---
+++ lib/widgets/new_item.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class NewItem extends StatefulWidget {
+  const NewItem({super.key});
+
+  @override
+  State<NewItem> createState() {
+    return _NewItem();
+  }
+}
+
+class _NewItem extends State<NewItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Add new item')),
+      body: const Padding(
+        padding: EdgeInsets.all(12),
+        child: Text('form...'),
+      ),
+    );
+  }
+}
