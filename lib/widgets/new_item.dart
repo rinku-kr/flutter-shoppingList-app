352b826c78ac54794c118602436345488a08e4ff
rinku-kr <rk24736599@gmail.com>
2023-12-31 04:31:30 +0530

    Adding drop down buttom

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:shopping_list/data/categories.dart';
+
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
 
@@ -27,6 +29,42 @@ class _NewItem extends State<NewItem> {
               validator: (value) {
                 return;
               },
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Quantity'),
+                    ),
+                    initialValue: '1',
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: DropdownButtonFormField(items: [
+                    for (final category in categories.entries)
+                      DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              Text(category.value.title),
+                            ],
+                          ))
+                  ], onChanged: (value) {}),
+                ),
+              ],
             )
           ],
         )),
