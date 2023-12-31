9f0f7ceac39349210f65c7709f5fad0217278dad
rinku-kr <rk24736599@gmail.com>
2023-12-31 17:54:36 +0530

    Sending a POST request to the backend

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
-import 'package:shopping_list/models/grocery_item.dart';
+// import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -24,12 +27,23 @@ class _NewItem extends State<NewItem> {
     if (_formkey.currentState!.validate()) {
       _formkey.currentState!.save();
 
-      Navigator.of(context).pop(GroceryItem(
-        id: DateTime.now().toString(),
-        name: _enteredName,
-        quantity: _enteredQuantity,
-        category: _selectedCategory,
-      ));
+      final url = Uri.https('flutter-app-767e4-default-rtdb.firebaseio.com',
+          'shopping_list.json');
+      http.post(url,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: json.encode({
+            'name': _enteredName,
+            'quantity': _enteredQuantity,
+            'category': _selectedCategory.title,
+          }));
+      // Navigator.of(context).pop(GroceryItem(
+      //   id: DateTime.now().toString(),
+      //   name: _enteredName,
+      //   quantity: _enteredQuantity,
+      //   category: _selectedCategory,
+      // ));
     }
   }
 
